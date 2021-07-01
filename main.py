770fbdf1de25739110d3ab5cb85216349c1fc92d
Kevin Lacker <lacker@gmail.com>
2021-07-01 16:13:13 -0700

    find candidate centers, remove dupes

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-from astropy import units
-from astropy.coordinates import Angle
 import json
 import math
+import numpy as np
 import redis
-import scipy
+from scipy.spatial import KDTree
 
 REDIS = redis.Redis(host="localhost", port=6379, db=0)
 
@@ -24,6 +23,7 @@ class Point(object):
         self.priority = priority
 
 
+# Parse the json into Point objects for convenience
 POINTS = [
     Point(*args)
     for args in zip(
@@ -35,8 +35,87 @@ POINTS = [
 ]
 
 
+def intersect_two_circles(x0, y0, r0, x1, y1, r1):
+    """
+    Finding the intersections of two circles.
+
+    Thanks to:
+    https://stackoverflow.com/questions/55816902/finding-the-intersection-of-two-circles
+    """
+    # circle 1: (x0, y0), radius r0
+    # circle 2: (x1, y1), radius r1
+
+    d = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+
+    if d > r0 + r1:
+        raise ValueError("non-intersecting")
+
+    if d < abs(r0 - r1):
+        raise ValueError("one circle within the other")
+
+    if d == 0 and r0 == r1:
+        raise ValueError("coincident circles")
+
+    a = (r0 ** 2 - r1 ** 2 + d ** 2) / (2 * d)
+    h = math.sqrt(r0 ** 2 - a ** 2)
+    x2 = x0 + a * (x1 - x0) / d
+    y2 = y0 + a * (y1 - y0) / d
+    x3 = x2 + h * (y1 - y0) / d
+    y3 = y2 - h * (x1 - x0) / d
+
+    x4 = x2 - h * (y1 - y0) / d
+    y4 = y2 + h * (x1 - x0) / d
+
+    return ((x3, y3), (x4, y4))
+
+
 def main():
-    pass
+    arr = np.array([[p.ra, p.decl] for p in POINTS])
+    tree = KDTree(arr)
+
+    # Find all pairs of points that could be captured by a single observation
+    pairs = tree.query_pairs(2 * RADIUS)
+    print("there are", len(pairs), "pairs")
+
+    # A list of (ra, decl) coordinates for the center of possible circles
+    candidate_centers = []
+
+    for i0, i1 in pairs:
+        p0 = POINTS[i0]
+        p1 = POINTS[i1]
+        # For each pair, find two points that are a bit less than RADIUS away from each point.
+        # These are the possible centers of the circle.
+        # TODO: make the mathematical argument of this algorithm's sufficiency clearer
+        r = 0.9999 * RADIUS
+        try:
+            c0, c1 = intersect_two_circles(p0.ra, p0.decl, r, p1.ra, p1.decl, r)
+            candidate_centers.append(c0)
+            candidate_centers.append(c1)
+        except ValueError:
+            continue
+
+    print("there are", len(candidate_centers), "candidates for circle-centers")
+    candidate_neighbors = tree.query_ball_point(
+        candidate_centers, RADIUS, return_sorted=True
+    )
+
+    # Filter out any candidates whose neighbors are the same as a previous candidate
+    centers = []
+    neighbors = []
+    seen = set()
+    for c, neighbor_list in zip(candidate_centers, candidate_neighbors):
+        key = tuple(neighbor_list)
+        if key in seen:
+            continue
+        seen.add(key)
+        centers.append(c)
+        neighbors.append(neighbor_list)
+
+    print(
+        "after removing functional duplicates, there are",
+        len(centers),
+        "possible circles",
+    )
 
 
 if __name__ == "__main__":
